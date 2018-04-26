dfc374c13336146c62d54c0886dc6b72392f9ef9
Daniel Castrillon Cuartas <jcastrd4@onjci.com>
2018-04-26 16:07:49 -0500

    Add antd layout

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -5,11 +5,14 @@ import Login from './views/Login';
 import PhotoEditor from './views/PhotoEditor';
 import PrivateRoute from './ui/PrivateRoute';
 
+import AppLayout from '../components/layout/AppLayout';
+
 class App extends Component {
   render() {
     return (
       <Switch>
         <Route exact path="/login" component={Login} />
+        <Route exact path="/layout" component={AppLayout} />
         <PrivateRoute exact path="/app" component={PhotoEditor} />
         <Redirect from="/" to="/login" />
       </Switch>
--- client/src/components/ui/PrivateRoute.jsx
+++ client/src/components/ui/PrivateRoute.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { Route, Redirect } from 'react-router';
 import { connect } from 'react-redux';
 
-function PrivateRoute({ component: Component, ...rest }) {
+function PrivateRoute({ component: Component, logged, ...rest }) {
   return (
     <Route {...rest} render={(props) => (
-      rest.logged
+      true//logged
         ? <Component  {...props} />
         : <Redirect to="/login" />
     )} />
